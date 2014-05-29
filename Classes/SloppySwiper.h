e77d038d2bed3605d18c83152402a5ddfd7255dd
Arkadiusz Holko <fastred@fastred.org>
2014-05-29 21:11:09 +0200

    Improve comments in the header file

--- Classes/SloppySwiper.h
+++ Classes/SloppySwiper.h
@@ -8,7 +8,10 @@
 
 @interface SloppySwiper : NSObject <UINavigationControllerDelegate>
 
-// Designated initializer if the class isn't set in the Interface Builder.
+/// Gesture recognizer used to recognize swiping to the right.
+@property (weak, nonatomic) UIPanGestureRecognizer *panRecognizer;
+
+/// Designated initializer if the class isn't used from the Interface Builder.
 - (instancetype)initWithNavigationController:(UINavigationController *)navigationController;
 
 @end
