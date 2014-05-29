7abe300060965fd6867fd236511e1bffd93c5764
Arkadiusz Holko <fastred@fastred.org>
2014-05-29 21:22:58 +0200

    Simplify the example project
    One view controller is just enough to show how the library works

--- Example/SloppySwiperExample/ViewController.m
+++ Example/SloppySwiperExample/ViewController.m
@@ -18,6 +18,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    NSUInteger stackCount = [self.navigationController.viewControllers count];
+
+    if (stackCount % 2 == 0) {
+        self.view.backgroundColor = [UIColor colorWithRed:0.921 green:0.929 blue:1.000 alpha:1.000];
+    }
+
+    self.title = [@(stackCount) stringValue];
 }
 
 - (void)didReceiveMemoryWarning
