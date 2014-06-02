625ced1822a5596a64e098b6d8ff2b4bab9027e7
Arkadiusz Holko <fastred@fastred.org>
2014-06-02 12:27:54 +0200

    Add SSWDirectionalPanGestureRecognizer

---
+++ Classes/SSWDirectionalPanGestureRecognizer.h
@@ -0,0 +1,25 @@
+//
+//  SSWDirectionalPanGestureRecognizer.h
+//  Pods
+//
+//  Created by Arkadiusz on 01-06-14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, SSWPanDirection) {
+    SSWPanDirectionRight,
+    SSWPanDirectionDown,
+    SSWPanDirectionLeft,
+    SSWPanDirectionUp
+};
+
+/**
+ *  `SSWDirectionalPanGestureRecognizer` is a subclass of `UIPanGestureRecognizer`. It adds `direction` property and checks if the pan gesture started in the correct direction; it fails otherwise.
+ */
+@interface SSWDirectionalPanGestureRecognizer : UIPanGestureRecognizer
+
+@property (nonatomic) SSWPanDirection direction;
+
+@end
