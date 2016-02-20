f4e1457a911e95c24684d1b296d0eb9d91b4158c
David Wu <davidwu226@hotmail.com>
2016-02-20 14:53:27 -0800

    Add Carthage support.

--- SloppySwiper/SloppySwiper-Framework.h
+++ SloppySwiper/SloppySwiper-Framework.h
@@ -14,3 +14,7 @@ FOUNDATION_EXPORT double SloppySwiperVersionNumber;
 FOUNDATION_EXPORT const unsigned char SloppySwiperVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SloppySwiper/PublicHeader.h>
+
+#import "SloppySwiper.h"
+#import "SSWAnimator.h"
+#import "SSWDirectionalPanGestureRecognizer.h"
